0f597f4b77d70718096f8ef0b276a8292e00cfda
Andy Kenway <AndyRuix1@gmail.com>
2024-02-22 11:46:14 -0500

    sheet definitions separated

--- src/lib/Sheet.ts
+++ src/lib/Sheet.ts
@@ -1,11 +1,7 @@
+import type { IAuthData, ITablePosition } from '../types/Sheet';
 import { google, sheets_v4 } from 'googleapis';
 import { GoogleAuth, JWT } from 'google-auth-library';
 
-interface IAuthData {
-    client_email: string;
-    private_key: string;
-    scopes: string[];
-};
 
 export default class Sheets {
     private sheets: sheets_v4.Sheets;
@@ -13,7 +9,7 @@ export default class Sheets {
     private gAuth: GoogleAuth;
     private currentSheetId: string = '';
     private currentSheetName: string = '';
-    private currentTablePosition: { letter: string; number: number } = { letter: 'A', number: 1 };
+    private currentTablePosition: ITablePosition = { letter: 'A', number: 1 };
 
     private gAuthCreds: IAuthData = {
         client_email: '',
@@ -63,7 +59,7 @@ export default class Sheets {
     };
 
     /**
-     * @description metodo para sumar letras a partir de una letra, ejemplo: (A, 2) -> C | (AA, 2) -> AC
+     * @description Método para sumar letras a partir de una letra, ejemplo: (A, 2) -> C | (AA, 2) -> AC
      * @param letter Letra la cual se quiere sumar.
      * @param count Numero de veces para sumar la letra
      * @returns Letra correspondiente a las sumas anteriores.
@@ -79,7 +75,7 @@ export default class Sheets {
 
     /**
      * @description Retorna el numero correspondiente al abecedario, en caso de superar la Z (ej: AA, AB, etc) se devuelve el numero correspondiente para una hoja de calcula. 
-     * @param letter Letra la cual requiere transformar a numerico.
+     * @param letter Letra la cual requiere transformar a numérico.
      * @return numero correspondiente a la letra.
      */
     private letterToNumber(letter: string): number {
@@ -94,11 +90,11 @@ export default class Sheets {
     };
 
     /**
-     * @description Formatea la posicion para hacerla mas manejable.
-     * @param position Posicion a formatear, ejemplo: B:4
+     * @description Formatea la posición para hacerla mas manejable.
+     * @param position Posición a formatear, ejemplo: B:4
      * @returns Se retorna un objeto separando la letra del numero.
      */
-    private getPosition(position: string): { letter: string, number: number } {
+    private getPosition(position: string): ITablePosition {
         return {
             letter: position.split(':')[0],
             number: parseInt(position.split(':')[1])
@@ -107,9 +103,9 @@ export default class Sheets {
 
     /**
      * @description obtiene la ultima columna con un valor. 
-     * @param positionLetter Letra de la posicion inicial de la tabla.
-     * @param positionNumber Numero de la posicion inicial de la tabla
-     * @returns Se retorna el numero del ultimo valor de una columna en la posicion indicada
+     * @param positionLetter Letra de la posición inicial de la tabla.
+     * @param positionNumber Numero de la posición inicial de la tabla
+     * @returns Se retorna el numero del ultimo valor de una columna en la posición indicada
      */
     private async getLastColumn(positionLetter: string, positionNumber: number): Promise<string> {
         let currentColumn = positionLetter;
@@ -144,10 +140,10 @@ export default class Sheets {
     };
 
     /**
-     * @description Obtener la posicion del ultimo valor conocido en una fila.
-     * @param positionLetter Letra de la posicion inicial de la tabla
-     * @param positionNumber Numero de la posicion inicial de la tabla
-     * @returns Se retorna el numero del ultimo valor de la fila en la posicion indicada.
+     * @description Obtener la posición del ultimo valor conocido en una fila.
+     * @param positionLetter Letra de la posición inicial de la tabla
+     * @param positionNumber Numero de la posición inicial de la tabla
+     * @returns Se retorna el numero del ultimo valor de la fila en la posición indicada.
      */
     private async getLastRow(positionLetter: string, positionNumber: number): Promise<number> {
         let currentColumn = positionLetter;
@@ -170,8 +166,8 @@ export default class Sheets {
     };
 
     /**
-     * @description metodo para preparar un objeto con datos para un arreglo, esto es necesario para hacer insercion en las tablas.
-     * @param initPosition Posicion de la tabla a trabajar (opcional si ya se aplico definicion desde la clase) 
+     * @description Método para preparar un objeto con datos para un arreglo, esto es necesario para hacer inserción en las tablas.
+     * @param initPosition Posición de la tabla a trabajar (opcional si ya se aplico definición desde la clase) 
      * @param values Datos a ser procesados.
      * @returns Arreglo con todos los datos preparados para ser insertados en su respectiva tabla
      */
@@ -194,9 +190,9 @@ export default class Sheets {
     };
 
     /**
-     * @description Metodo para preparar valores.
+     * @description Método para preparar valores.
      * @param values Objeto de los valores a trabajar 
-     * @returns {T} Objeto con sus valores formateados (string numerico > numeros enteros)
+     * @returns {T} Objeto con sus valores formateados (string numérico > números enteros)
      */
     private formatValues<T>(values: T): T {
         const valuesObject = { ...values };
@@ -211,8 +207,8 @@ export default class Sheets {
     };
 
     /**
-     * @description metodo para obtener la posicion de la hoja entre las demas.
-     * @returns Se obtiene el numero de la hoja (su posicion exacta entre todas las hojas)
+     * @description Método para obtener la posición de la hoja entre las demás.
+     * @returns Se obtiene el numero de la hoja (su posición exacta entre todas las hojas)
      */
     private async getSheetIdBySheetName(): Promise<number> {
         const sheetResponse = await this.sheets.spreadsheets.get({ spreadsheetId: this.currentSheetId });
@@ -229,9 +225,9 @@ export default class Sheets {
     };
 
     /**
-     * @description Metodo para obtener todsa las cabeceras de una tabla.
-     * @param initialPosition Posicion inicial de la tabla a trabajar: Ej: A:2 (opcional si ya se agrego en la instancia).
-     * @returns Cabeceras de la tabla completa desde el inicio hasta la posicion con valor conocida.
+     * @description Método para obtener todas las cabeceras de una tabla.
+     * @param initialPosition Posición inicial de la tabla a trabajar: Ej: A:2 (opcional si ya se agrego en la instancia).
+     * @returns Cabeceras de la tabla completa desde el inicio hasta la posición con valor conocida.
      */
     public async getTableHeaders(initialPosition?: string): Promise<string[]> {
         const position = initialPosition ? this.getPosition(initialPosition) : this.currentTablePosition;
@@ -244,8 +240,8 @@ export default class Sheets {
     };
 
     /**
-     * @description Metodo para cambiar la posicion de la tabla la cual se esta trabajando. Primero se ingresa la letra, separada por dos puntos del numero, ejemplo: H:13
-     * @param positionTable Posicion inicial de la tabla.  Formato: A:1 (opcional si ya se agrego en la instancia)
+     * @description Método para cambiar la posición de la tabla la cual se esta trabajando. Primero se ingresa la letra, separada por dos puntos del numero, ejemplo: H:13
+     * @param positionTable Posición inicial de la tabla.  Formato: A:1 (opcional si ya se agrego en la instancia)
      * @returns {this}
      */
     public changeTableInitPosition(positionTable: string): this {
@@ -254,7 +250,7 @@ export default class Sheets {
     };
 
     /**
-     * @description Metodo para cambiar el ID de la hoja de calculo la cual esta siendo trabajda.
+     * @description Método para cambiar el ID de la hoja de calculo la cual esta siendo trabajada.
      * @param sheetId ID de la hoja de calculo. Esta puede ser encontrada en el URL
      * @returns {this}
      */
@@ -264,7 +260,7 @@ export default class Sheets {
     };
 
     /**
-     * @description Metodo para cambiar el nombre de la hoja de calculo la cual esta siendo trabajada.
+     * @description Método para cambiar el nombre de la hoja de calculo la cual esta siendo trabajada.
      * @param sheetName Nombre exacto de la hoja de calculo.
      * @returns {this}
      */
@@ -274,7 +270,7 @@ export default class Sheets {
     };
 
     /**
-     * @description Metodo para cambiar el nombre de la hoja de calculo y su ID  en una unica funcion.
+     * @description Método para cambiar el nombre de la hoja de calculo y su ID  en una única función.
      * @param sheetName Nombre exacto de la hoja de calculo.
      * @param sheetId  ID de la hoja de la hoja de calculo.
      * @returns {this}
@@ -286,8 +282,8 @@ export default class Sheets {
     };
 
     /**
-     * @description Metodo para obtener todos los valores de una tabla, se pueden filtrar.
-     * @param initialPosition Posicion inicial de la tabla a trabajar.
+     * @description Método para obtener todos los valores de una tabla, se pueden filtrar.
+     * @param initialPosition Posición inicial de la tabla a trabajar.
      * @param filter Filtro para obtener los valores de la tabla filtrados. 
      * @returns {T[]} Todos los valores de la tabla indicada.
      */
@@ -319,8 +315,8 @@ export default class Sheets {
     };
 
     /**
-     * @description Metodo para insertar uno o mas valores a una tabla por medio de un arreglo de objetos.
-     * @param initPosition Posicion inicial de la tabla a trabajar. Formato: A:1 (Opcional si ya se agrego en la instancia)
+     * @description Método para insertar uno o mas valores a una tabla por medio de un arreglo de objetos.
+     * @param initPosition Posición inicial de la tabla a trabajar. Formato: A:1 (Opcional si ya se agrego en la instancia)
      * @param values Valores a insertar a la tabla indicada por medio de un objeto.
      */
     public async insertValues<T>({ initPosition, values }: { initPosition?: string, values: T[] }) {
@@ -338,9 +334,9 @@ export default class Sheets {
     };
 
     /**
-     * @description Metodo para actualizar valores de la tabla por medio de un filtro. 
-     * @param initPosition Posicion inicial de la tabla a trabajar. Formato: A:1 (Opcional si ya se agrego en la instancia)
-     * @param values Valores que tendran las filas afectadas por el filtro.
+     * @description Método para actualizar valores de la tabla por medio de un filtro. 
+     * @param initPosition Posición inicial de la tabla a trabajar. Formato: A:1 (Opcional si ya se agrego en la instancia)
+     * @param values Valores que tendrán las filas afectadas por el filtro.
      * @param filter Filtro con el que se buscaran las filas y afectar.
      */
     public async updateValues<T>({ initPosition, filter, valuesUpdate }: { initPosition?: string, filter: { (val: T): boolean }, valuesUpdate: T }): Promise<void> {
@@ -374,8 +370,8 @@ export default class Sheets {
     };
 
     /**
-     * @description Metodo para eliminar filas por medio de un filtro.
-     * @param initPosition Posicion inicial de la tabla a trabajar. Formato: A:1 (Opcional si ya se agrego en la instancia)
+     * @description Método para eliminar filas por medio de un filtro.
+     * @param initPosition Posición inicial de la tabla a trabajar. Formato: A:1 (Opcional si ya se agrego en la instancia)
      * @param filter Filtro con el que se buscaran las filas y eliminarlas.
      */
     public async deleteRowsByFilter<T>({ initPosition, filter }: { initPosition?: string, filter: { (val: T): boolean } }) {
@@ -394,7 +390,7 @@ export default class Sheets {
         const tableHeaders = await this.getTableHeaders(initPosition);
         const endLetter = this.sumLetter(position.letter, tableHeaders.length + 1);
 
-        for (let row of rowsToDelete.reverse()) {
+        for (const row of rowsToDelete.reverse()) {
             await this.sheets.spreadsheets.batchUpdate({
                 spreadsheetId: this.currentSheetId,
                 requestBody: {
---
+++ src/types/Sheet.d.ts
@@ -0,0 +1,10 @@
+export interface IAuthData {
+    client_email: string;
+    private_key: string;
+    scopes: string[];
+}
+
+export interface ITablePosition {
+    letter: string;
+    number: number;
+}
